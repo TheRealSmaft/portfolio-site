a90be0b6d3e3c8378221b024f006e10c4b915a24
TheRealSmaft <mrsmaft@gmail.com>
2017-04-13 10:44:43 -0500

    Portfolio Page
    Made a PortfolioPiece component and set up a basic layout for the
    Portfolio Page.

--- app/styles/pages/index.js
+++ app/styles/pages/index.js
@@ -1,2 +1,3 @@
 export { default as LoadingPageStyles } from './Loading/Loading.css';
 export { default as HomePageStyles } from './Home/Home.css';
+export { default as PortfolioPageStyles } from './Portfolio/Portfolio.css';
