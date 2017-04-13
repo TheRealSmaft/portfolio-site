3e47966949698a48e1537a04665706c15ec1f6a7
TheRealSmaft <mrsmaft@gmail.com>
2017-04-13 11:33:20 -0500

    About Page Grid

--- app/components/Pages/About/index.js
+++ app/components/Pages/About/index.js
@@ -1,12 +1,174 @@
 import React from 'react';
 
-import { ResponsiveContainer } from '../../Containers';
+import { ResponsiveContainer, Grid, Row, Col} from '../../Containers';
+
+import { AboutPageStyles } from '../../../styles/pages';
 
 const AboutPage = React.createClass({
 	render() {
 		return (
 			<ResponsiveContainer>
-
+				<h1>
+					ABOUT ME
+				</h1>
+				<Grid
+					breakPoints={[992, 768]}
+					gutter={4}
+				>
+					<Row blocks={5}>
+						<Col
+							blocks={2}
+							breaks={[35, 100]}
+						>
+							<p>
+								Responsive circle with a pic of me (or Sinister Silhouette)
+							</p>
+						</Col>
+						<Col
+							blocks={3}
+							breaks={[65, 100]}
+						>
+							<p>
+								About me paragraph
+							</p>
+						</Col>
+					</Row>
+					<Row>
+						<Col
+							breaks={[40, 100]}
+						>
+							<p>
+								Responsive circle with a brain graphic
+							</p>
+						</Col>
+						<Col
+							breaks={[32, 52]}
+						>
+							<ul>
+								<li>
+									List
+								</li>
+								<li>
+									of
+								</li>
+								<li>
+									brain
+								</li>
+								<li>
+									skills
+								</li>
+							</ul>
+						</Col>
+						<Col
+							breaks={[28, 48]}
+						>
+							<ul>
+								<li>
+									List
+								</li>
+								<li>
+									of
+								</li>
+								<li>
+									brain
+								</li>
+								<li>
+									skills
+								</li>
+							</ul>
+						</Col>
+					</Row>
+					<Row>
+						<Col
+							breaks={[40, 100]}
+						>
+							<p>
+								Responsive circle with a heart graphic
+							</p>
+						</Col>
+						<Col
+							breaks={[32, 52]}
+						>
+							<ul>
+								<li>
+									List
+								</li>
+								<li>
+									of
+								</li>
+								<li>
+									heart
+								</li>
+								<li>
+									skills
+								</li>
+							</ul>
+						</Col>
+						<Col
+							breaks={[28, 48]}
+						>
+							<ul>
+								<li>
+									List
+								</li>
+								<li>
+									of
+								</li>
+								<li>
+									heart
+								</li>
+								<li>
+									skills
+								</li>
+							</ul>
+						</Col>
+					</Row>
+					<Row>
+						<Col
+							breaks={[40, 100]}
+						>
+							<p>
+								Responsive circle with a hand graphic
+							</p>
+						</Col>
+						<Col
+							breaks={[32, 52]}
+						>
+							<ul>
+								<li>
+									List
+								</li>
+								<li>
+									of
+								</li>
+								<li>
+									hand
+								</li>
+								<li>
+									skills
+								</li>
+							</ul>
+						</Col>
+						<Col
+							breaks={[28, 48]}
+						>
+							<ul>
+								<li>
+									List
+								</li>
+								<li>
+									of
+								</li>
+								<li>
+									hand
+								</li>
+								<li>
+									skills
+								</li>
+							</ul>
+						</Col>
+					</Row>
+				</Grid>
 			</ResponsiveContainer>
 		)
 	}
--- app/styles/pages/index.js
+++ app/styles/pages/index.js
@@ -2,3 +2,4 @@ export { default as LoadingPageStyles } from './Loading/Loading.css';
 export { default as HomePageStyles } from './Home/Home.css';
 export { default as PortfolioPageStyles } from './Portfolio/Portfolio.css';
 export { default as ContactPageStyles } from './Contact/Contact.css';
+export { default as AboutPageStyles } from './About/About.css';
