1dfd59a94a79e35d8a278104f9c202b1cc7e322c
Matthew Brubaker Smith <mrsmaft@gmail.com>
2017-05-15 15:56:50 -0500

    Workin on Animations

--- app/components/Pages/Contact/index.js
+++ app/components/Pages/Contact/index.js
@@ -4,13 +4,86 @@ import ReactDOM from 'react-dom';
 import { ResponsiveContainer, Grid, Row, Col, DeferredEventExecutor } from '../../Containers';
 import { SVG, Rect } from '../../Containers/ShapeContainers';
 
+import BodyMovin from '../../../plugins/bodymovin.min';
+
 const ContactPage = React.createClass({
+	componentDidMount() {
+		var animationData = {
+			animationData: require('../../../assets/images/interactables/UFO/UFOArrival.json'),
+			path: '../../../assets/images/interactables/UFO',
+			loop: false,
+			autoplay: false,
+			name: 'ufoArrival',
+			renderer: 'svg' ,
+			container: ReactDOM.findDOMNode(this.refs.alienContact)
+		};
+
+		this.ufoArrival = BodyMovin.loadAnimation(animationData);
+		this.ufoArrival.addEventListener('complete', this.ufoHover);
+		setTimeout(() => {
+			this.ufoArrival.play();
+		}, 500);
+	},
+
+	componentWillUpdate() {
+		this.svgHeight = this.refs.alienContact.getBoundingClientRect().height;
+	},
+
+	ufoHover() {
+		this.ufoArrival.removeEventListener('complete', this.ufoHover);
+		this.ufoArrival.destroy();
+
+		var animationData = {
+			animationData: require('../../../assets/images/interactables/UFO/UFOHovering.json'),
+			path: '../../../assets/images/interactables/UFO',
+			loop: true,
+			autoplay: true,
+			name: 'ufoHover',
+			renderer: 'svg' ,
+			container: ReactDOM.findDOMNode(this.refs.alienContact)
+		};
+
+		this.ufoHover = BodyMovin.loadAnimation(animationData);
+		this.ufoHover.setSpeed(.25);
+
+		var carrotAnimationData = {
+			animationData: require('../../../assets/images/interactables/UFO/Carrot.json'),
+			path: '../../../assets/images/interactables/UFO',
+			loop: true,
+			autoplay: true,
+			name: 'ufoHover',
+			renderer: 'svg' ,
+			container: ReactDOM.findDOMNode(this.refs.carrot)
+		};
+
+		this.carrot = BodyMovin.loadAnimation(carrotAnimationData);
+
+		setTimeout(() => {
+			this.useTractorBeam();
+		}, 1000)
+	},
+
+	useTractorBeam() {
+		var animationData = {
+			animationData: require('../../../assets/images/interactables/UFO/TractorBeam.json'),
+			path: '../../../assets/images/interactables/UFO',
+			loop: false,
+			autoplay: true,
+			name: 'tractorBeam',
+			renderer: 'svg' ,
+			container: ReactDOM.findDOMNode(this.refs.tractorBeam)
+		};
+
+		this.tractorBeam = BodyMovin.loadAnimation(animationData);
+	},
+
 	render() {
 		return (
 			<ResponsiveContainer>
 				<h1>
 					CONTACT ME
 				</h1>
+
 				<Grid
 					breakPoints={[768]}
 					gutter={4}
@@ -19,15 +92,48 @@ const ContactPage = React.createClass({
 						<Col
 							breaks={[100]}
 						>
-							<p>
-								This will be a pic of a flying saucer (from Zanahoria) abducting the Sinister Silhouette.
-							</p>
+							<div
+								style={{
+									height: this.svgHeight + 'px'
+								}}
+							>
+								<div
+									style={{
+										position: 'relative'
+									}}
+								>
+									<div
+										ref="carrot"
+										style={{
+											position: 'absolute',
+											top: '0px',
+											left: '0px'
+										}}
+									>
+									</div>
+									<div
+										ref="alienContact"
+										style={{
+											position: 'absolute',
+											top: '0px',
+											left: '0px'
+										}}
+									>
+									</div>
+									<div
+										ref="tractorBeam"
+										style={{
+											position: 'absolute',
+											top: '0px',
+											left: '0px'
+										}}
+									>
+									</div>
+								</div>
+							</div>
 						</Col>
 						<Col
 							breaks={[100]}
-							style={{
-								position: 'relative'
-							}}
 						>
 							<SVG
 								title="Form Background"
@@ -59,15 +165,6 @@ const ContactPage = React.createClass({
 								/>
 								</Rect>
 							</SVG>
-							<p
-								style={{
-									position: 'absolute',
-									top: '5%',
-									left: '8%'
-								}}
-							>
-								This will be a contact form
-							</p>
 						</Col>
 					</Row>
 				</Grid>
