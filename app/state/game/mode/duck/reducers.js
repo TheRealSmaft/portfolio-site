c14261cbe2316aca181c94b5eac152efe361ec7a
TheRealSmaft <mrsmaft@gmail.com>
2017-06-13 15:54:59 -0500

    Update reducers.js

--- app/state/game/mode/duck/reducers.js
+++ app/state/game/mode/duck/reducers.js
@@ -4,7 +4,7 @@ import PasswordRandomizer from '../../../../assets/images/items/Paper/passwordRa
 const modeReducer = (state = {
 	gameMode: true,
 	password: PasswordRandomizer.getPassword(),
-	progressLevel: 8,
+	progressLevel: 0,
 	justBeatGame: false
 }, action) => {
 	switch(action.type) {
